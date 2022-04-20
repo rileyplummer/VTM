6e43af7339407f4227c56a1544768fe70f59f755
rileyplummer <riley-plummer@uiowa.edu>
2022-04-20 12:28:05 -0500

    troubleshooting radius

--- tests/unit/test_estimate.py
+++ tests/unit/test_estimate.py
@@ -1,11 +1,11 @@
 import app
 
-def test_add_inputs(height, radius):
+def test_add_inputs(radius,height):
     """
     GIVEN a user enters the radius and height
     WHEN the function calculates using these amounts
     THEN the total cost is calculated
 
     """
-    assert app.add_inputs(360,180) == 141300
+    assert app.add_inputs(180,360) == 141300.00000000003
 
