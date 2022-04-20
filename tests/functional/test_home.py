a815b76caee8fbbc7bbe57d3c0d3e58777ed3d3b
rileyplummer <riley-plummer@uiowa.edu>
2022-04-20 10:22:10 -0500

    Adding functional py testing

---
+++ tests/functional/test_home.py
@@ -0,0 +1,53 @@
+def test_index_route(app,client):
+    """
+    Given a Flask application configured for testing
+    WHEN the '/' route is requested (GET)
+    THEN check that the response is valid
+    """
+    with app.test_client() as test_client:
+        res = test_client.get('/')
+        assert res.status code == 200
+        assert b'Vertical Tank Maintenance' in res.data
+
+def test_about_route(app,client):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/about' route is requested (GET)
+    THEN check that the response is valid
+    """
+    with app.test_client() as test_client:
+        res = test_client.get('/about')
+        assert res.status_code == 200
+        assert b"About Vertical Tank Maintenance" in res.data
+
+
+def test_estimate_route(app,client):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/estimate' route is requested (GET)
+    THEN check that the response is valid
+    """
+    with app.test_client() as test_client:
+        res = test_client.get('/estimate')
+        assert res.status_code == 200
+        assert b"Create An Estimate" in res.data
+
+def test_add_inputs_route(app,client):
+      """
+    GIVEN a Flask application configured for testing
+    WHEN the '/add_inputs' route is requested (GET)
+    THEN check that the response is valid/a number appears
+    """
+    with app.test_client() as test_client:
+        res = test_client.get('/add_inputs')
+        assert rest.status_code == 200
+
+def test_add_inputs_route(app,client):
+      """
+    GIVEN a Flask application configured for testing
+    WHEN the '/add_inputs' route is requested (GET)
+    THEN check that the response is valid/a number appears
+    """
+    with app.test_client() as test_client:
+        res = test_client.post('/add_inputs')
+        assert rest.status_code == 200
