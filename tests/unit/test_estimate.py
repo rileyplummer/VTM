e985bc795ff8c78fe25cd94167ed020523c37b63
rileyplummer <riley-plummer@uiowa.edu>
2022-04-22 11:02:54 -0500

    unit testing

--- tests/unit/test_estimate.py
+++ tests/unit/test_estimate.py
@@ -1,13 +1,11 @@
-from pickletools import read_unicodestring1
 import app 
 
 
-def test_add_inputs():
+def test_add_inputs(app, client):
     """
     GIVEN a user enters the radius and height
     WHEN the function calculates using these amounts
     THEN the total cost is calculated
     """
-    radius = 180
-    height = 360
-    assert app.add_inputs() == 141300.00000000003
+    print(" -- add_inputs unit test")
+    assert app.add_inputs(180,360) == 141300.00000000003
