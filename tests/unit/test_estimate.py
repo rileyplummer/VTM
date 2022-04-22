c0999ca678f8e76c0d50a6a4ca2245b3c08d3c05
rileyplummer <riley-plummer@uiowa.edu>
2022-04-22 11:33:08 -0500

    Update test_estimate.py

--- tests/unit/test_estimate.py
+++ tests/unit/test_estimate.py
@@ -1,11 +1,10 @@
 import app 
 
 
-def test_add_inputs(app, client):
+def test_add_inputs():
     """
     GIVEN a user enters the radius and height
     WHEN the function calculates using these amounts
     THEN the total cost is calculated
     """
-    print(" -- add_inputs unit test")
     assert app.add_inputs(180,360) == 141300.00000000003
