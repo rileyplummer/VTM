13a17ab2f451b85ee5cec2e0f90cf745ece090eb
rileyplummer <riley-plummer@uiowa.edu>
2022-04-25 13:04:28 -0500

    Fixed unit test for estimates

--- tests/unit/test_estimate.py
+++ tests/unit/test_estimate.py
@@ -1,10 +1,12 @@
-import app 
-
-
-def test_add_inputs():
+def test_add_inputs(app,client):
     """
     GIVEN a user enters the radius and height
     WHEN the function calculates using these amounts
     THEN the total cost is calculated
     """
-    assert app.add_inputs(180,360) == 141300.00000000003
+    with app.test_client() as test_client:
+        # pass in the data use Chrome Developer Tools -> Network -> Click on page -> Payload
+        calculate = {"radius":"180", "height":"360"} 
+        res = test_client.post('/add_inputs', data=calculate)
+        assert res.status_code == 200 
+        assert b"141300.00000000003" in res.data 
